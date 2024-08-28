0ba67103dfed9e694da1e3b9f47ff4c27c2db42e
Hirofumi Horikawa <hori.kawa.shima.tool+github@gmail.com>
2024-08-28 11:21:57 +0000

    denestize

--- src/content.js
+++ src/content.js
@@ -1,8 +1,10 @@
-// Get current URL
-const currentUrl = window.location.href;
+(async () => {
+  // Get current URL
+  const currentUrl = window.location.href;
 
-// Check if the page(https://learn.microsoft.com/ja-jp) is in Japanese
-if (currentUrl.startsWith("https://learn.microsoft.com/ja-jp/")) {
+  // Check if the page(https://learn.microsoft.com/ja-jp) is in Japanese
+  if (!currentUrl.startsWith("https://learn.microsoft.com/ja-jp/")) return;
+
   // Translate URL to English
   const englishUrl = currentUrl.replace("/ja-jp/", "/en-us/");
 
@@ -11,33 +13,28 @@ if (currentUrl.startsWith("https://learn.microsoft.com/ja-jp/")) {
     'time[aria-label="記事のレビュー日"]'
   );
 
-  if (japaneseDateElement) {
-    // Get English page and parse update date
-    fetch(englishUrl)
-      .then((response) => response.text())
-      .then((data) => {
-        // Parse HTML in English page
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, "text/html");
-        const englishDateElement = doc.querySelector(
-          'time[aria-label="Article review date"]'
-        );
-        const englishDate = englishDateElement
-          ? englishDateElement.innerText
-          : null;
+  if (!japaneseDateElement) return;
 
-        if (englishDate) {
-          // Add update info to Japanese page
-          const updateInfo = document.createElement("p");
-          updateInfo.className = "text-color"; // <class="text-color"> is defined in CSS
-          updateInfo.style.marginTop = "0"; // <p> default margin-top is 1rem
-          updateInfo.style.marginLeft = "1px"; // <p> default margin-left is 0
-          updateInfo.innerHTML = `英語版の更新日: <a href="${englishUrl}" target="_blank" class="text-color">${englishDate}</a>`;
-          japaneseDateElement.parentElement.appendChild(updateInfo);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching English page:", error);
-      });
+  try {
+    // Get English page and parse update date
+    const response = await fetch(englishUrl);
+    const data = await response.text();
+
+    // Parse HTML in English page
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, "text/html");
+    const englishDate = doc.querySelector('time[aria-label="Article review date"]')?.innerText;
+
+    if (!englishDate) return;
+
+    // Add update info to Japanese page
+    const updateInfo = document.createElement("p");
+    updateInfo.className = "text-color"; // <class="text-color"> is defined in CSS
+    updateInfo.style.marginTop = "0"; // <p> default margin-top is 1rem
+    updateInfo.style.marginLeft = "1px"; // <p> default margin-left is 0
+    updateInfo.innerHTML = `英語版の更新日: <a href="${englishUrl}" target="_blank" class="text-color">${englishDate}</a>`;
+    japaneseDateElement.parentElement.appendChild(updateInfo);
+  } catch (error) {
+    console.error("Error fetching English page:", error);
   }
-}
+})();
