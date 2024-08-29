676e47bcb029acdb89f2eb004febd1bfe41542b3
koudaiii <cs006061@gmail.com>
2024-08-29 14:16:56 +0900

    Supported theme color

--- src/content.js
+++ src/content.js
@@ -30,24 +30,45 @@ if (currentUrl.startsWith("https://learn.microsoft.com/ja-jp/")) {
         if (englishDate) {
           // Get Japanese date
           const japaneseDate = new Date(japaneseDateElement.innerText);
-          // Add update info to Japanese page
+
+          // Check for theme based on data-theme-to and aria-pressed="true"
+          const selectedThemeButton = document.querySelector('button[data-theme-to][aria-pressed="true"]');
+
+          // Set default browser text color
+          textColorClass = "text-color";
+
+          // if theme is selected, apply appropriate text color based on theme
+          if (selectedThemeButton) {
+            const themeAttribute = selectedThemeButton.getAttribute('data-theme-to');
+            if (themeAttribute === "dark") {
+              textColorClass = "text-color-dark";
+            } else if (themeAttribute === "high-contrast") {
+              textColorClass = "text-color-high-contrast";
+            } else if (themeAttribute === "light") {
+              textColorClass = "text-color-light";
+            }
+          }
+
+          // Add update info to current page
           const updateInfo = document.createElement("p");
+
           // Add icon to update info
-          icon = "";
+          informationIcon = "";
+
           // Compare English date and Japanese date
           if (englishDate > japaneseDate || debug === "true") {
             // Display alert if English page is updated
             updateInfo.className = "alert is-primary"; // <class="alert is-primary"> is defined in CSS
             updateInfo.style.margin = "5px";
             updateInfo.style.padding = "10px";
-            icon = `<span class="icon"><span class="docon docon-status-error-outline" aria-hidden="true" style="margin: 0px"></span></span>`;
+            informationIcon = `<span class="icon"><span class="docon docon-status-error-outline" aria-hidden="true" style="margin: 0px"></span></span>`;
           } else {
             // Display info if English page is not updated
             updateInfo.style.marginTop = "0"; // <p> default margin-top is 1rem
             updateInfo.style.marginLeft = "3px"; // <p> default margin-left is 0
-            updateInfo.className = "text-color"; // <class="text-color"> is defined in CSS
+            updateInfo.className = textColorClass; // Apply appropriate text color based on theme
           }
-          updateInfo.innerHTML = icon + `英語版の更新日: <a href="${englishUrl}" target="_blank" class="text-color">${englishDate.toLocaleDateString()}</a>`;
+          updateInfo.innerHTML = informationIcon + `英語版の更新日: <a href="${englishUrl}" target="_blank" class="${textColorClass}">${englishDate.toLocaleDateString()}</a>`;
           japaneseDateElement.parentElement.appendChild(updateInfo);
         }
       })
